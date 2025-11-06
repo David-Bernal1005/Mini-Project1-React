71ed9d43c3c49c68981f2f0ca56221445b3b1a40
David <esteban.b.200805@gmail.com>
2025-11-06 06:43:50 -0500

    Trabajo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 importante: debe tener slash inicial y final
+  base: '/Mini-Project1-React/', // 👈 importante: debe tener slash inicial y final
 })
+const nombre = "Mariana";
+const edad = 26;
+const ciudad = "Bogotá";
+const carrera = "Diseño Gráfico";
+
+const mensaje = `
+Hola ${nombre}, tienes ${edad} años y vives en ${ciudad}.
+Estás inscrita en la carrera de ${carrera}.
+${edad >= 18 ? "Eres mayor de edad." : "Eres menor de edad."}
+Bienvenida a la plataforma.
+`;
+
+console.log(mensaje);
